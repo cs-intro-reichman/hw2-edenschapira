c25231ab3ca4656f74d80561ba79d9d776fd4d8f
Eden Schapira <eden.schapira@post.runi.ac.il>
2025-11-09 23:05:10 +0200

    cheers test1

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,23 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String cheer = args[0];
+        int timesToCheer = Integer.parseInt(args[1]);
+        String needToBeAn = "AaEeFfHhIiLlMmNnOoRrSsXx";
+        int i = 0;
+        while (i<cheer.length()) {
+                char c = cheer.charAt(i);
+                if(needToBeAn.indexOf(c)==-1){
+                        System.out.println("Give me a " +c +": " +c +"!");
+                } else {
+                        System.out.println("Give me an " +c +": " +c +"!");
+                }
+                i++;                
+        }
+        System.out.println("What does that spell?");
+        for(int times=0; times<timesToCheer; times++){
+                System.out.println(cheer +"!!!");
+        }        
+	    
         }
 }
